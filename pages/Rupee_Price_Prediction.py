ddaedf1596df4a045a945e794fedf4afb0676869
Mahantesh Hiremath <mahanteshimath@gmail.com>
2025-04-08 00:16:15 +0530

    test

--- pages/Rupee_Price_Prediction.py
+++ pages/Rupee_Price_Prediction.py
@@ -176,39 +176,3 @@ if st.button("Generate Long-Term Forecast"):
 if st.checkbox("Show Raw Data"):
     st.subheader("Raw Data")
     st.write(df.head(10))
-
-
-
-st.markdown(
-    '''
-    <style>
-    .streamlit-expanderHeader {
-        background-color: blue;
-        color: white; # Adjust this for expander header color
-    }
-    .streamlit-expanderContent {
-        background-color: blue;
-        color: white; # Expander content color
-    }
-    </style>
-    ''',
-    unsafe_allow_html=True
-)
-
-footer="""<style>
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: #2C1E5B;
-color: white;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p>Developed with ❤️ by <a style='display: inline; text-align: center;' href="https://iitj-ml-learnings.streamlit.app/" target="_blank">Srijit and Mahantesh</a></p>
-</div>
-"""
-st.markdown(footer,unsafe_allow_html=True)  
